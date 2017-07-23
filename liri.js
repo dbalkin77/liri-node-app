23434e9ff20ccfabb82503c002e99e6ec02e5f08
Douglas Balkin <dbalkin77@gmail.com>
2017-07-23 10:47:51 -0500

    app requires

---
+++ liri.js
@@ -0,0 +1,17 @@
+// Require all NPM Packages
+
+var twitter = require("twitter");
+var spotify = require("spotify");
+var request = require("request");
+var keys = require('./keys.js');
+
+// request('http://www.google.com', function (error, response, body) {
+//   console.log('error:', error); // Print the error if one occurred 
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+//   console.log('body:', body); // Print the HTML for the Google homepage. 
+// });
+
+// console.log(keys.twitterKeys.access_token_key);
+
+// node liri.js my-tweets ////////////////////////////////////////////////
+
