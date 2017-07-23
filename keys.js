4e585593ad94268e9ccc45eebf0664284f348441
Douglas Balkin <dbalkin77@gmail.com>
2017-07-22 19:42:02 -0500

    added twitter API keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '4zvpB3sPnTfXiJFjGz5gIxgBv',
+  consumer_secret: 'Sd3sHZS5D6OQMQ54NYWQKsmAYSoTjDNFywohpr82G93neRpsv3',
+  access_token_key: '888913684457771008-3w9HGg28FCPt3CVAGmrlNJQTiKF2497',
+  access_token_secret: 'zX3fx5U85xU4Hxlo8oWzyLEPtk0QJn87YeJs9GJkj302Y',
+}
